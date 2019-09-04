577011b590038b09a9144092951b550a4ab5e6ae
Eradash <poirier.20.100@hotmail.com>
2019-09-04 10:25:13 -0400

    try return statement

--- typing.rb
+++ typing.rb
@@ -34,6 +34,10 @@ ENV['SLACK_API_TOKENS'].split.each do |token|
   client.on(:user_typing) do |data|
     logger.info data
 
+    if data.channel == "U1NR0R8TD"
+      return
+    end
+
     if data.user != "U8QUCDX0U" && data.user != "U0AUB0FT6" && data.channel != "CDBA2T69W"
       client.typing channel: data.channel
     end
